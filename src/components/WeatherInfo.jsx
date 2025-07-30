36e0a4bbb45b42caaac4eca997374b8e37abb677
PuneetVerma07 <pk2382849@gmail.com>
2025-07-30 16:08:14 +0530

    icons add

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,19 +1,40 @@
 import React from "react";
+import {
+  WiDaySunny,
+  WiCloudy,
+  WiRain,
+  WiSnow,
+  WiThunderstorm,
+  WiFog,
+} from "react-icons/wi";
 
-// const conditionIcons = {
-//   Clear : 
-// }
+const conditionIcons = {
+  Clear: <WiDaySunny size={48} className="text-yellow-400" />,
+  Clouds: <WiCloudy size={48} className="text-gray-400" />,
+  Rain: <WiRain size={48} className="text-blue-400" />,
+  Snow: <WiSnow size={48} className="text-blue-200" />,
+  Thunderstorm: <WiThunderstorm size={48} className="text-purple-500" />,
+  Fog: <WiFog size={48} className="text-gray-300"/>,
+};
 
 const WeatherInfo = ({ weather }) => {
+
+  const {temp, condition, humidity, name, country } = weather;
+  const icon = conditionIcons[condition]  || <WiDaySunny size={48}/>
+
   return (
-    <div className="glass p-6 shadow-lg w-full max-w-md rounded-2xl text-center text-white space-y-2 bg-opacity-20">
-      <h2 className="text-xl font-semibold">
-        {weather.name},{weather.country}
-      </h2>
-      <p className="text-3xl mt-2">{weather.temp} °C</p>
-      <p className="capitalize text-xl">{weather.condition}</p>
-      <p className="text-xl">Humidity : {weather.humidity}%</p>
-      
+    <div className="mt-6 p-4 rounded-lg shadow-md bg-gray-100 dark:bg-gray-800">
+      <div className="flex justify-between items-center">
+        <div>
+          <h2 className="text-2xl font-semibold">{name}, {country}</h2>
+          <p className="text-xl font-semibold text-gray-400 dark:text-gray-300">{condition}</p>
+        </div>
+        <div>{icon}</div>
+      </div>
+      <div className="mt-4">
+        <p className="text-xl font-semibold">Tempratur : {temp}&deg;C</p>
+        <p className="text-xl font-semibold">Humidity : {humidity}%</p>
+      </div>
     </div>
   );
 };
